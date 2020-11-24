3df5a0e946ac736c0c58443e9102028f39b09635
SirPaulAgbola <paul.agbola@gmail.com>
2020-11-24 13:05:37 -0800

    web2

---
+++ image/Javascript/script.js
@@ -0,0 +1,115 @@
+// // use case of innerHTML
+// document.getElementById("dom").innerHTML = 1 + 1;
+
+// // use of case of document.write()
+// document.write(2 + 2);
+
+// // use case of window.alert()
+// window.alert(10 + 10);
+
+// // use case of console.log()
+// console.log(0 + 1);
+
+// variables
+var name = "Paul";
+var hometown = "kame";
+var complexion = "fair";
+var haircolor = "dark";
+var eyecolor = "brown";
+var sex = "male";
+var petname = "Snobby";
+var age = 20;
+
+// document.getElementById("dom").innerHTML =
+//   "my name is" + name + "and I am" + age + "years old";
+
+console.log("hello");
+
+var location;
+var name;
+var age;
+
+Location = "Ho";
+name = "Paul";
+age = "20";
+
+console.log("20");
+
+// Javascript operators
+// =,+,-,*,/,%,--,++, +=
+// assign values to variables and add them together
+
+// var x = 20;
+// var y = 10;
+// var z = x + y;
+// console.log(z);
+
+// var x = 60;
+// x += 10;
+// console.log(x);
+
+// string operators and string concatenation
+// var student1 = "kaka";
+// var student2 = "ronaldo";
+// var both = student1 + "" + student2;
+
+// document.getElementById("dom").innerHTML = both;
+
+// var x = 6;
+// var y = "10" + 5;
+// var z = "string";
+
+document.getElementById("dom1").innerHTML = x;
+document.getElementById("dom2").innerHTML = y;
+document.getElementById("dom3").innerHTML = z;
+
+// Javascript comparison operators
+//  == equal to
+//  === value and data type
+//  != not equal
+//  ! == not equal, value and type
+//  > greater than
+//  < less than
+//  >= greater than or equal
+//  <= less than or equal to
+//  && logical and
+//  || logical or
+//  ! logical not
+
+// var x = 20;
+// var y = 10;
+// var z = x - y;
+// console.log(z);
+
+// var x = 20;
+// var y = 10;
+// var z = x * y;
+// console.log(z);
+
+// var x = 20;
+// var y = 10;
+// var z = x / y;
+// console.log(z);
+
+// var x = 20;
+// var y = 10;
+// var z = x + y;
+// console.log(z);
+
+// increment operator
+var x = 7;
+x++;
+var z = x;
+document.getElementById("dom1").innerHTML = x;
+
+//  decrement
+var x = 7;
+x--;
+var y = x;
+document.getElementById("dom2").innerHTML = y;
+
+// modulus
+var x = 30;
+var y = 20;
+var z = x % y;
+document.getElementById("dom3").innerHTML = z;
