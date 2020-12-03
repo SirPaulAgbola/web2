da80ae82aded44defc95963f33cd161378122ae6
SirPaulAgbola <paul.agbola@gmail.com>
2020-12-02 17:05:27 -0800

    web2

---
+++ image/Javascript/conditions.js
@@ -0,0 +1,24 @@
+// var age = 22;
+// if (age < 3) {
+//   alert("your age is less than 3");
+// } else {
+//   alert("your age is greater than 3");
+// }
+
+// var age = prompt("enter your age");
+// if (age == 18) {
+//     alert("You are eligible to vote");
+// } else if (age > 18) {
+//     alert("you are more than qualified to vote");
+// } else {
+//     alert("You are not eligible to vote");
+// }
+
+var age = prompt("enter your age");
+if (age == 18) {
+  alert("You are eligible to vote");
+} else if (age > 18) {
+  alert("your age is" + age + "You are more than qualified to vote");
+} else {
+  alert("your age is" + age + "You are not eligible to vote");
+}
---
+++ image/Javascript/tuesdat.js
@@ -0,0 +1,44 @@
+// Built in methods
+// alert
+// console.log
+// prompt
+// clear
+
+// alert("hello world");
+
+// prompt("what is your name");
+
+// var yourname = prompt("what is your name");
+// alert("your name is" + yourname);
+
+// var signin = prompt("sign in here");
+// alert("sign in here" + signin);
+
+// var age;
+// var fname;
+// var locate;
+
+// age = prompt("what is your age?");
+// fname = prompt("what is your name?");
+// locate = prompt("what is your location");
+
+// document.getElementById("dom1").innerHTML = "Your age is " + age;
+// document.getElementById("dom2").innerHTML = "Your name is" + fname;
+// document.getElementById("dom3").innerHTML = "Your location is" + locate;
+
+// booliean logic
+var x = 5;
+var y = 9;
+console.log(x > 10);
+console.log(x >= 5);
+console.log(x < -5);
+console.log(x <= 100);
+console.log(x == "5");
+console.log(x != "b");
+console.log(x != "5");
+console.log(x === "5");
+console.log(x !== "5");
+console.log(x === "5");
+console.log(x < 10 && x != 5);
+console.log(y > 9 || x === 5);
+console.log(!(x === y));
